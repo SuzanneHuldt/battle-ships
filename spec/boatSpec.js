16745539a7eee0655443af16ba8a51bd8051848b
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2017-12-14 21:08:38 +0000

    halfway through illegal placement implementation

--- spec/boatSpec.js
+++ spec/boatSpec.js
@@ -8,24 +8,83 @@ describe("Boat", function(){
     it("launches a boat with zero hits", function(){
       expect(boat.hits).toEqual(0)
     });
-    it("launches a boat with given dimensions", function(){
-      boat.chooseSize(3)
+    it("launches a boat with correct dimensions for type SUBMARINE", function(){
+      boat.chooseType('submarine')
       expect(boat.size).toEqual(3)
+    });
+    it("launches a boat with correct dimensions for type DESTROYER", function(){
+      boat.chooseType('destroyer')
+      expect(boat.size).toEqual(2)
+    });
+    it("launches a boat with correct dimensions for type CRUISER", function(){
+      boat.chooseType('cruiser')
+      expect(boat.size).toEqual(3)
+    });
+    it("launches a boat with correct dimensions for type BATTLESHIP", function(){
+      boat.chooseType('battleship')
+      expect(boat.size).toEqual(4)
+    });
+    it("launches a boat with correct dimensions for type CARRIER", function(){
+      boat.chooseType('carrier')
+      expect(boat.size).toEqual(5)
     });
   });
 
   describe("place boat", function(){
     it("adds the cells where the boat is placed into occupied cells", function(){
-      boat.placeBoat('1,3')
-      expect(boat.occupiedCells[0]).toEqual('1,3')
+      boat.chooseType('destroyer')
+      boat.placeBoat('A,3')
+      expect(boat.occupiedCells[0]).toEqual('A,3')
+    });
+    it("does not place more cells than the size of the boat", function(){
+      boat.chooseType('destroyer')
+      boat.placeBoat('A,3')
+      boat.placeBoat('A,4')
+      expect(boat.placeBoat('A,5')).toEqual('error')
+    });
+    it("does not place more cells than the size of the boat", function(){
+      boat.chooseType('destroyer')
+      boat.placeBoat('A,3')
+      boat.placeBoat('A,4')
+      boat.placeBoat('A,5')
+      expect(boat.occupiedCells.length).toEqual(2)
     });
   });
 
   describe("floating status", function(){
     it("shows whethere boat has sunk", function(){
-      boat.chooseSize(3)
+      boat.chooseType('cruiser')
       boat.hits = 3
       expect(boat.isSunk()).toEqual(true)
     });
   });
+
+  describe("take hit", function(){
+    it("removes cell when hit", function(){
+      boat.chooseType('destroyer')
+      boat.placeBoat('A,3')
+      boat.placeBoat('A,4')
+      boat.hitCell('A,3')
+      expect(boat.occupiedCells.length).toEqual(1)
+    });
+  });
+
+  describe("correct boat placement", function(){
+    it("doesn't allow diagonal boat placement", function(){
+      boat.chooseType('destroyer')
+      boat.placeBoat('A,3')
+      expect(boat.placeBoat('B,2')).toEqual('error')
+    });
+  });
+
+  describe("returns whether boat placement is legal", function(){
+    it("returns illegal placement for diagonal cell placement", function(){
+      boat.chooseType('destroyer')
+      boat.placeBoat('A,3')
+      expect(boat.legalPlacement('B,2')).toEqual(false)
+    });
+  });
+
+
+
 });
--- src/boat.js
+++ src/boat.js
@@ -3,16 +3,60 @@ function Boat(){
   this.occupiedCells = []
 }
 
-Boat.prototype.chooseSize = function(size){
-  this.size = size
+Boat.prototype.getSize = function(size){
+  return this.size = size
 }
 
 Boat.prototype.placeBoat = function(cell){
-  this.occupiedCells.push(cell)
+  if(this.cellsFree()) {
+    this.occupiedCells.push(cell)
+  }
+  else {
+    return 'error'
+  }
 }
 
 Boat.prototype.isSunk = function(){
     if(this.hits === this.size){
       return true
     }
+}
+
+Boat.prototype.cellsFree = function(){
+  return this.size > this.occupiedCells.length
+}
+
+Boat.prototype.hitCell = function(cell){
+  var index = this.occupiedCells.indexOf(cell)
+  this.occupiedCells.splice(index, 1)
+}
+
+Boat.prototype.legalPlacement = function(cell){
+
+}
+
+Boat.prototype.splitCell = function(cell){
+  var dimensions = cell.split("")
+  var alpha = dimensions[0]
+  var num = dimensions[1]
+}
+
+Boat.prototype.chooseType = function(type){
+  switch(type) {
+    case 'submarine':
+      this.size = 3;
+      break;
+    case 'destroyer':
+      this.size = 2;
+      break;
+    case 'carrier':
+      this.size = 5;
+      break;
+    case 'battleship':
+      this.size = 4;
+      break;
+    case 'cruiser':
+      this.size = 3;
+      break;
+  }
 }
