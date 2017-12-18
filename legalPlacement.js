7b8a3eda884474f38d9a966aa9fcf97c65175c14
Suzanne Huldt <suzanne.huldt.16@ucl.ac.uk>
2017-12-18 23:02:40 +0000

    legal placement fixed

--- legalPlacement.js
+++ legalPlacement.js
@@ -1,19 +1,27 @@
+(function(exports){
+  function legalPlacement(oldCell, newCell){
+    splitCell(newCell)
+    var newNum = parseInt(dimensions[1])
+    var newLetter = dimensions[0]
+    var newLetter = newLetter.charCodeAt(0)
 
+    splitCell(oldCell)
+    var oldNum = parseInt(dimensions[1])
+    var oldLetter = dimensions[0]
+    var newLetter = oldLetter.charCodeAt(0)
 
-legalPlacement = function(oldCell, newCell){
-  splitCell(newCell)
-  var newNum = parseInt(dimensions[1])
-  var newLetter = dimensions[0]
-  var newLetter = newLetter.charCodeAt(0)
+    var numDiff = oldNum - newNum
+    var letterDiff = oldLetter - newLetter
 
-  splitCell(oldCell)
-  var oldNum = parseInt(dimensions[1])
-  var oldLetter = dimensions[0]
-  var newLetter = oldLetter.charCodeAt(0)
+    return ((numDiff + letterDiff) > 1)
 
-  
-}
+  }
+  exports.legalPlacement = legalPlacement;
+})(this);
 
-splitCell = function(cell){
-  return dimensions = cell.split(",")
-}
+(function(exports){
+  function splitCell(cell){
+    return dimensions = cell.split(",")
+  }
+  exports.splitCell = splitCell;
+})(this);
